1c3a96f81218430ec667a0454d0310faffde00d1
Imee Cuison <imeetwelve@gmail.com>
2015-02-03 22:59:32 -0500

    first commit

---
+++ popup.js
@@ -0,0 +1,24 @@
+function getChuck () {
+ $.ajax({
+    url: 'http://api.icndb.com/jokes/random/',
+
+    type: 'GET',
+    datatype: 'JSONP',
+    success: function (data) {
+      //console.log(instaPage.memeConfig.baseurl + "_Select_ByTrending";
+      console.log(data);
+      _.each(data, function(item, idx, arr) {
+      // _.each(item, function (currentItem, index, array) {
+      //  _.each(currentItem, function (value, index, array){
+            $('body').append('<p>' + item.joke + '</p>');
+//});
+//  });
+});
+  },
+      error: function (error) {
+      console.log(error);
+    }
+  })
+}
+
+getChuck();
