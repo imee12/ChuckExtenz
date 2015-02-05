37a053b6b5d98c975557a6f0bbe3e73c84995c33
Imee Cuison <imeetwelve@gmail.com>
2015-02-04 23:46:51 -0500

    works

--- popup.js
+++ popup.js
@@ -7,13 +7,13 @@ function getChuck () {
     success: function (data) {
       //console.log(instaPage.memeConfig.baseurl + "_Select_ByTrending";
       console.log(data);
-      _.each(data, function(item, idx, arr) {
+    //  _.each(data, function(item, idx, arr) {
       // _.each(item, function (currentItem, index, array) {
       //  _.each(currentItem, function (value, index, array){
-            $('body').append('<p>' + item.joke + '</p>');
+            $('body').append('<p>' + data.value.joke + '</p>');
 //});
 //  });
-});
+//});
   },
       error: function (error) {
       console.log(error);
